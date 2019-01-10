14e89727331a9fd980bac04dd2e9e3210a561a03
Sky_Scraper <moinsoheb02@gmail.com>
2019-01-10 11:31:29 +0530

    Added keras method

--- one_hot_encoding.py
+++ one_hot_encoding.py
@@ -46,4 +46,18 @@ print(onehot_encoded)
 inverted = label_encoder.inverse_transform([argmax(onehot_encoded[0, :])])
 print(inverted)
 
+# to_categorical in KERAS
+
+from numpy import array
+from numpy import argmax
+from keras.utils import to_categorical
+# define example
+data = [1, 3, 2, 0, 3, 2, 2, 1, 0, 1]
+data = array(data)
+print(data)
+# one hot encode
+encoded = to_categorical(data)
+print(encoded)
+# invert encoding
+inverted = argmax(encoded[0])
 
